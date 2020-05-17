e734760dcb7c15ffa22851414c8452c5e2525274
Athénaïs Roussel <athenais.roussel@epfedu.fr>
2020-05-18 00:27:55 +0200

    QR Code v.1

--- app/src/main/java/fr/epf/moov/QRCodeActivity.kt
+++ app/src/main/java/fr/epf/moov/QRCodeActivity.kt
@@ -1,13 +1,38 @@
 package fr.epf.moov
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.Result
+import me.dm7.barcodescanner.zxing.ZXingScannerView
 
-class QRCodeActivity  : AppCompatActivity(){
+
+class QRCodeActivity  : AppCompatActivity(), ZXingScannerView.ResultHandler  {
+    private var scannerView : ZXingScannerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qrcode)
+        scannerView = ZXingScannerView(this)
+        setContentView(scannerView)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        scannerView?.setResultHandler(this)
+        scannerView?.startCamera()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        scannerView?.stopCamera()
+    }
+
+    override fun handleResult(rawResult: Result?) {
+        Log.v("QR Code", rawResult?.text) // Prints scan results
+        Log.v("QR Code", rawResult?.barcodeFormat.toString()) // Prints the scan format (qrcode, pdf417 etc.)
+
+        scannerView?.resumeCameraPreview(this)
 
     }
 }
